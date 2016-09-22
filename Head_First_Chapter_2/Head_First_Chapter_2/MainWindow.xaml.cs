e6eff052811ac80a05bc1220910d640633b5012b
Jade Vance <jadenicolevance@gmail.com>
2016-09-22 11:42:37 -0700

    initial commit for example two. Added stub methods for button clicks

--- Head_First_Chapter_2/Head_First_Chapter_2/MainWindow.xaml.cs
+++ Head_First_Chapter_2/Head_First_Chapter_2/MainWindow.xaml.cs
@@ -24,5 +24,30 @@ namespace Head_First_Chapter_2
         {
             InitializeComponent();
         }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            int number = 15;
+            number = number + 10;
+            number = 36 * 15;
+            number = 12 - (42 / 7);
+            number += 10;
+            number *= 3;
+            number = 71 / 3;
+
+            int count = 0;
+            count ++;
+            count--;
+
+            string result = "hello";
+            result += " again " + result;
+            output.Text = result;
+            result = "the value is: " + count;
+            result = "";
+
+            bool yesNo = false;
+            bool anotherBool = true;
+            yesNo = !anotherBool;
+        }
     }
 }
